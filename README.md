20e623c2b6cf2ebf123b9c911b4283c14d599a63
name <williangel10@outlook.com>
2024-01-17 15:48:29 -0400

    solicitudes basicas(cambios antes de empezar la maquetacion del proyecto)

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ rutas como api.com/algo
 ### query parameters
 
 informacion extra para los endpoints
+
+### http status code
+
+
+### api key
+
+
+
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //query parameters ?limit=3
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3'
-
-
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&live_Wd3AwYuFZGxxEioUKOxems72lcmbT7zlZLwTrrST64OTlnR9NEOAzxDKkRNQoh4O'
+//https://api.thecatapi.com/v1/images/search?api_key=YOUR_API_KEY
+//const API_KEY = live_Wd3AwYuFZGxxEioUKOxems72lcmbT7zlZLwTrrST64OTlnR9NEOAzxDKkRNQoh4O;
 
 //sin async-await
 // function refresh(){
@@ -16,6 +16,7 @@ const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3'
 async function refresh(){
   const res = await fetch(API_URL);
   const data = await res.json();
+  console.log(data);
   const img = document.querySelector('img');
   img.src = data[0].url;
 } 
