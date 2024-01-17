47a1caa7c99d6833f967441f11566fb4b800e90d
name <williangel10@outlook.com>
2024-01-17 16:01:14 -0400

    agregada la estructura basica del html

--- main.js
+++ main.js
@@ -17,8 +17,15 @@ async function refresh(){
   const res = await fetch(API_URL);
   const data = await res.json();
   console.log(data);
-  const img = document.querySelector('img');
-  img.src = data[0].url;
+  
+  const img1 = document.getElementById('img1');
+  const img2 = document.getElementById('img2'); 
+  const img3 = document.getElementById('img3'); 
+
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
+
 } 
 
 
